dbbdf1f039ea4f329120e399f410bf14c7b257c2
Aryan-0 <81675278+Aryan-0@users.noreply.github.com>
2022-06-28 12:58:38 +0530

    Add files via upload

---
+++ calculater.py
@@ -0,0 +1,27 @@
+import statistics
+
+import numpy
+import math
+print("welcome to pocket calculator")
+print("while u enter two numbers, u will get the addition, subtraction, multiplication, division... to start Say hello!. ")
+if input(""):
+    print("enter your first number")
+    k = int(input())
+    print("enter your second number")
+    l = int(input())
+    lk = k+l
+    tt = k-l
+    gk = k*l
+    fp = k/l
+    print("addition : ",lk)
+    print("subtraction :" ,tt)
+    print("multiplication :" ,gk)
+    print("division :",fp)
+
+#if input("multiply"):
+#    print("enter your first number")
+#    k = int(input())
+#    print("enter your second number")
+#    l = int(input())
+#    hk = k*l    
+#    print(hk)
